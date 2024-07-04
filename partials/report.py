03cc9255c5e042ec8df7d2b6a3c443a2199ff139
Josué dos Santos <josue.santos@svm.com.br>
2024-07-04 09:02:44 -0300

    add metric

--- partials/report.py
+++ partials/report.py
@@ -31,19 +31,23 @@ def report():
     title_installments = filtered_df.loc[title_installments.index]
     
     value_installments = title_installments['amount'].sum()
+    size_installments = title_installments['amount'].count()
+
 
     title_counts = filtered_df['title'].value_counts().head(5)
     title_sums = filtered_df.groupby('title')['amount'].sum().loc[title_counts.index]
+    title_count = filtered_df.groupby('title')['amount'].count().loc[title_counts.index]
     title_sums_formatted = title_sums.apply(format_money)
+
     
-    kpi1, kpi2, kpi3 = st.columns(3)
+    kpi1, kpi2, kpi3, kpi4 = st.columns(4)
     kpi1.metric(
         label="Total " + option_month_ref,
         value=format_money(total_month)
     )
 
     kpi2.metric(
-        label="Total de Parcelamentos",
+        label="Parcelamentos",
         value=format_money(value_installments)
     )
 
@@ -52,14 +56,44 @@ def report():
         value="{:.2f}%".format(value_installments / total_month * 100)
     )
 
-    kpi1.metric(
-        label="- Parcelamentos",
+    kpi4.metric(
+        label="Total - Parcelamentos",
         value=format_money(total_month - value_installments)
     )
 
-    kpi1.metric(
-        label="- " + title_counts.index[0],
+    kpi2.metric(
+        label= title_counts.index[0],
+        value=format_money(title_sums[0])
+    )
+
+    kpi3.metric(
+        label="Percentual de Comprometimento",
+        value="{:.2f}%".format((title_sums[0] + value_installments) / total_month * 100)
+    )
+
+    kpi4.metric(
+        label="Total - " + title_counts.index[0],
         value=format_money(total_month - title_sums[0] - value_installments)
+    )
+
+    kpi2.metric(
+        label= "Total de Movimentações",
+        value=filtered_df['amount'].count() - size_installments
+    )
+
+    kpi3.metric(
+        label="Movimentações " + title_counts.index[0],
+        value=title_count[0]
+    )
+
+    kpi4.metric(
+        label="Diferentes Movimentações",
+        value=filtered_df['amount'].count() - title_count[0] - size_installments
+    )
+
+    kpi4.metric(
+        label="Movimentações por Dia",
+        value="{:.2f}".format((filtered_df['amount'].count() - title_count[0] - size_installments) / 30)
     )
 
     result_df = pd.DataFrame({
