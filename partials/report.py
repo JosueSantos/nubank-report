91bdfde911567fb21ca94684f2b5e59a9d713b06
Josué dos Santos <josue.santos@svm.com.br>
2024-10-08 10:14:14 -0300

    change for files in memory

--- partials/report.py
+++ partials/report.py
@@ -2,64 +2,72 @@
 import streamlit as st
 from services.FilesService import FilesService
 from services.ReportService import ReportService
+from io import StringIO
+import pandas as pd
 
 
 def report():
     st.markdown('### &#x25A3; Análise de Gastos por Mês')
     
-    data = FilesService.load_and_process_data('files')
+    uploaded_file = st.file_uploader("Escolha um arquivo CSV", type="csv")
+    if uploaded_file is not None:
+        string_data = uploaded_file.getvalue().decode("utf-8")
+        data = StringIO(string_data)
+        
+        df = pd.read_csv(data)
+        df['date'] = pd.to_datetime(df['date'])
+        df = df[df['category'] != 'payment']
+        df = df.sort_values(by='date')
 
-    option_month_ref = st.selectbox('Selecione um mês:', data['month_ref'].sort_values(ascending=False).unique())
+        total_month, value_installments, percentMonthSubInstallments, maxTitleSum, percentMonthSubInstallmentsInTitle, totalMov, maxTitle, maxTitleCount, movDaily, filtered_df, result_df = ReportService.buildInfo(df)
+        
+        kpi1, kpi2, kpi3 = st.columns(3)
+        kpi1.metric(
+            label="Total ",
+            value=ReportService.format_money(total_month)
+        )
 
-    total_month, value_installments, percentMonthSubInstallments, maxTitleSum, percentMonthSubInstallmentsInTitle, totalMov, maxTitle, maxTitleCount, movDaily, filtered_df, result_df = ReportService.buildInfo(option_month_ref, data)
-    
-    kpi1, kpi2, kpi3 = st.columns(3)
-    kpi1.metric(
-        label="Total " + option_month_ref,
-        value=ReportService.format_money(total_month)
-    )
+        kpi2.metric(
+            label="Parcelamentos",
+            value=ReportService.format_money(value_installments)
+        )
 
-    kpi2.metric(
-        label="Parcelamentos",
-        value=ReportService.format_money(value_installments)
-    )
+        kpi3.metric(
+            label="Percentual de Comprometimento",
+            value="{:.2f}%".format(percentMonthSubInstallments)
+        )
 
-    kpi3.metric(
-        label="Percentual de Comprometimento",
-        value="{:.2f}%".format(percentMonthSubInstallments)
-    )
+        kpi1.metric(
+            label="Movimentações " + maxTitle,
+            value=maxTitleCount
+        )
 
-    kpi1.metric(
-        label="Movimentações " + maxTitle,
-        value=maxTitleCount
-    )
+        kpi2.metric(
+            label= "Soma dos valores do " + maxTitle,
+            value=ReportService.format_money(maxTitleSum)
+        )
 
-    kpi2.metric(
-        label= "Soma dos valores do " + maxTitle,
-        value=ReportService.format_money(maxTitleSum)
-    )
+        kpi3.metric(
+            label="Percentual de Comprometimento",
+            value="{:.2f}%".format(percentMonthSubInstallmentsInTitle)
+        )
 
-    kpi3.metric(
-        label="Percentual de Comprometimento",
-        value="{:.2f}%".format(percentMonthSubInstallmentsInTitle)
-    )
+        kpi1.metric(
+            label= "Total de Movimentações",
+            value=totalMov
+        )
 
-    kpi1.metric(
-        label= "Total de Movimentações",
-        value=totalMov
-    )
+        kpi2.metric(
+            label="Movimentações por Dia",
+            value="{:.2f}".format(movDaily)
+        )
 
-    kpi2.metric(
-        label="Movimentações por Dia",
-        value="{:.2f}".format(movDaily)
-    )
-
-    col1, col2 = st.columns(2)
-    with col1:
-        st.markdown("##### Compras Recorrentes")
-        st.dataframe(result_df)
-    with col2:
-        st.markdown('##### Dados Carregados:')
-        st.write(filtered_df)
-    
+        col1, col2 = st.columns(2)
+        with col1:
+            st.markdown("##### Compras Recorrentes")
+            st.dataframe(result_df)
+        with col2:
+            st.markdown('##### Dados Carregados:')
+            st.write(filtered_df)
+        
     
--- services/ReportService.py
+++ services/ReportService.py
@@ -45,3 +45,35 @@ class ReportService():
 
         return total_month, value_installments, percentMonthSubInstallments, maxTitleSum, percentMonthSubInstallmentsInTitle, totalMov, maxTitle, maxTitleCount, movDaily, filtered_df, result_df
     
+    def buildInfo(data):
+        filtered_df = data
+    
+        title_installments = filtered_df['title'].apply(ReportService.count_installments)
+        title_installments = title_installments[title_installments != 0]
+        title_installments = filtered_df.loc[title_installments.index]
+        
+        size_installments = title_installments['amount'].count()
+
+        title_counts = filtered_df['title'].value_counts()
+        title_sums = filtered_df.groupby('title')['amount'].sum().loc[title_counts.index]
+        title_count = filtered_df.groupby('title')['amount'].count().loc[title_counts.index]
+        title_sums_formatted = title_sums.apply(ReportService.format_money)
+
+        result_df = pd.DataFrame({
+            'Title': title_counts.index,
+            'Number of Occurrences': title_counts.values,
+            'Total Amount': title_sums_formatted.values
+        })
+        
+        total_month = data['amount'].sum()
+        value_installments = title_installments['amount'].sum()
+        percentMonthSubInstallments = value_installments / total_month * 100
+        maxTitleSum = title_sums[0]
+        percentMonthSubInstallmentsInTitle = (title_sums[0] + value_installments) / total_month * 100
+        totalMov = filtered_df['amount'].count() - size_installments
+        maxTitle = title_counts.index[0]
+        maxTitleCount = title_count[0]
+        movDaily = (filtered_df['amount'].count() - size_installments) / 30
+
+        return total_month, value_installments, percentMonthSubInstallments, maxTitleSum, percentMonthSubInstallmentsInTitle, totalMov, maxTitle, maxTitleCount, movDaily, filtered_df, result_df
+    
